cdbbfb7aae2e4b934655d3f3b5aeac32a20cae17
Ludriano Washington <ludinho.washi@gmail.com>
2025-03-11 11:52:02 -0300

    fix: including enum

---
+++ src/main/java/com/omega/cashflow/enumeration/TipoEnum.java
@@ -0,0 +1,12 @@
+package com.omega.cashflow.enumeration;
+
+public enum TipoEnum {
+  ENTRADA("ENTRADA"),
+  SAIDA("SAIDA");
+
+  private String displayName;
+
+  TipoEnum(String displayName) {
+    this.displayName = displayName;
+  }
+}
